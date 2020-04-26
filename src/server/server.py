150506b28dd4ef2b8f26d3b7ec18eb0766fce011
mitchduf <mitchduffield@hotmail.com>
2020-04-26 05:21:54 -0700

    Add unity executable and updated server file

--- src/server/server.py
+++ src/server/server.py
@@ -3,20 +3,9 @@ import webservices
 from flask import Flask, json, request
 
 import base64
-import mysql.connector
-
-companies = [{"id": 1, "name": "Company One"}, {"id": 2, "name": "Company Two"}]
 
 
 api = Flask(__name__)
-
-
-sql_db = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    passwd="",
-    database="database_name"
-)
 
 def _request_data(request):
     if not request.data:
@@ -35,30 +24,19 @@ def _request_form(request):
     return loaded_data
 
 
-@api.route('/star_map/new', methods=['POST'])
+@api.route('/starmap/new', methods=['POST'])
 def update_star_map():
     request_data = _request_form(request)
     if not request_data:
-        return {'success': False, 'failureInfo': 'No request data'}, 400
+        return {'success': False, 'failureInfo': 'Invalid request data'}, 400
 
-    return webservices.update_star_map(request_data)
+    return webservices.create_star_map(request_data)
 
 
 @api.route('/healthcheck', methods=['GET'])
 def ws_health_check():
     return json.dumps({'status': 200})
 
-@api.route('/')
-def hello_world():
-        return 'Hello, World!'
-
-@api.route('/companies', methods=['GET'])
-def get_companies():
-    return json.dumps(companies)
-
-@api.route('/companies', methods=['POST'])
-def post_companies():
-    return json.dumps({"success": True}), 201
 
 if __name__ == '__main__':
     api.run()
--- src/server/webservices.py
+++ src/server/webservices.py
@@ -170,3 +170,33 @@ def create_star_map(request_data):
     star_map.write_texture(resolution, path)
 
     return json.dumps({"success": True})
+
+
+def create_star_map(request_data):
+    num_stars_str = request_data.get("numStars")
+    resolution_x_str = request_data.get("resolutionX")
+    resolution_y_str = request_data.get("resolutionY")
+    path = request_data.get("path")
+    edges_str = request_data.get("edges")
+    vertices_str = request_data.get("vertices")
+    
+    if not num_stars_str or not resolution_x_str or not resolution_y_str or not path or not edges_str or not vertices_str:
+        return json.dumps({"success": False, "failureInfo": "Request data missing"}), 400
+
+    try:
+        resolution = (int(resolution_x_str), int(resolution_y_str))
+        num_stars = int(num_stars_str)
+    except ValueError:
+        return json.dumps({"success": False, "failureInfo": "Invalid resolution/num_stars format"}), 400
+    
+    try:
+        edges = json.loads(edges_str)
+        vertices = json.loads(vertices_str)
+    except json.JSONDecodeError:
+        return json.dumps({"success": False, "failureInfo": "Invalid edges/vertices format"}), 400
+
+    star_map = StarMap(num_stars)
+    star_map.add_edges(drawing_edges_to_star_edges(edges, match(star_map, vertices, edges)))
+    star_map.write_texture(resolution, path)
+
+    return json.dumps({"success": True}), 201
