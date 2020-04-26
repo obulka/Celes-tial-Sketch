c1d984b4c0d48416912279f25a0d2917d9a1a206
Owen Bulka <obulka@hotmail.com>
2020-04-26 05:34:56 -0700

    created run file

--- src/program.py
+++ src/program.py
@@ -12,17 +12,26 @@ from server.webservices import StarMap
 def main():
     """
     """
-    base_dir = os.path.join(
-        os.path.dirname(os.path.realpath(__file__)),
-        os.pardir,
-    )
-    star_map = create_star_map({
-        "numStars": 200,
-        "path": os.path.join(base_dir, "texture.png"),
-        "resolution": (500, 1000),
-        "edges": [(0, 1), (1, 2), (2, 3), (3, 4), (4, 0)],#[(0,1),(1,2),(2,3),(3,0)],
-        "vertices": [[490, 437], [336, 134], [670, 270], [297, 294], [613, 116]], #[(1,1),(2,1),(2,2),(1,2)],
-    })
+    base_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)))
+    app_path = os.path.join(base_dir, "Citrus.exe")
+    server_path = os.path.join(base_dir, "server", "server.py")
+
+    try:
+        os.system("python3 {}".format(server_path))
+        os.system(app_path)
+        while True:
+            time.sleep(3)
+
+    except KeyboardInterrupt as error:
+        print(error)
+
+    # star_map = create_star_map({
+    #     "numStars": 200,
+    #     "path": os.path.join(base_dir, "texture.png"),
+    #     "resolution": (500, 1000),
+    #     "edges": [(0, 1), (1, 2), (2, 3), (3, 4), (4, 0)],#[(0,1),(1,2),(2,3),(3,0)],
+    #     "vertices": [[490, 437], [336, 134], [670, 270], [297, 294], [613, 116]], #[(1,1),(2,1),(2,2),(1,2)],
+    # })
 
 
 if __name__ == "__main__":
